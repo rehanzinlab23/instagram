09064d5563cc5bf60afaf5f1dded0aee5762f3f7
rehanzinlab23 <rehan.zinlab@gmail.com>
2025-10-20 11:02:10 +0500

    now redirect

--- main.js
+++ main.js
@@ -52,9 +52,14 @@ const postImages = document.querySelectorAll('.post-main-image');
 
 // Login Page
 
-  if (localStorage.getItem("loggedIn") !== "true") {
-    window.location.href = "insta-login/insta-login.html";
+document.addEventListener("DOMContentLoaded", () => {
+  const loggedIn = localStorage.getItem("loggedIn");
+
+  if (loggedIn !== "true") {
+    window.location.replace("insta-login/insta-login.html");
   }
+});
+
 
 // Heart Overlay Effect
 
